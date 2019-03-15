e298e075c58d1508ab9a2b8e16e5c01604419133
jinxingfei <17864215770@163.com>
2019-03-15 17:52:22 +0800

    3-15

--- HousePriceAnalysis/PriceAnalysis/urls.py
+++ HousePriceAnalysis/PriceAnalysis/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     # localhost:8001/???/
         #为你的 URL 取名能使你在 Django 的任意地方唯一地引用它，尤其是在模板中。
     path('', views.index, name='index'),
-    path('show', views.showHouse, name='showHouse'),  # base :
+    path('show/<str:province>', views.showHouse, name='showHouse'),  # base :
     path('home', views.homePage, name='homePage'),
-    path('show/<int:value>/', views.showHouse2, name='showHouse2'),  # base :
+
     path('historyPrice/', views.showHouse3, name='showHouse3'),
 ]
